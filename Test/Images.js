8eb5521c2faa1685d6a5148f8a6c19df9a8049a6
Karsten Lindenburg <102625@glr.nl>
2025-01-30 16:02:57 +0000

    test

---
+++ Test/Images.js
@@ -0,0 +1,29 @@
+const heroImages = {
+  moira: "https://static.playoverwatch.com/img/pages/heroes/moira/portrait.png",
+  zarya: "https://static.playoverwatch.com/img/pages/heroes/zarya/portrait.png",
+  zenyatta:
+    "https://static.wikia.nocookie.net/overwatch_gamepedia/images/1/1a/OW2_Dva.png/revision/latest?cb=20241108201346",
+  roadhog:
+    "https://static.wikia.nocookie.net/overwatch_gamepedia/images/1/1a/OW2_Dva.png/revision/latest?cb=20241108201346",
+  reaper:
+    "https://static.playoverwatch.com/img/pages/heroes/reaper/portrait.png",
+  mei: "https://static.playoverwatch.com/img/pages/heroes/mei/portrait.png",
+  pharah:
+    "https://static.playoverwatch.com/img/pages/heroes/pharah/portrait.png",
+  reinhardt:
+    "https://static.playoverwatch.com/img/pages/heroes/reinhardt/portrait.png",
+  cassidy:
+    "https://static.playoverwatch.com/img/pages/heroes/cassidy/portrait.png",
+  torbjorn:
+    "https://static.playoverwatch.com/img/pages/heroes/torbjorn/portrait.png",
+  brigitte:
+    "https://static.playoverwatch.com/img/pages/heroes/brigitte/portrait.png",
+  tracer:
+    "https://static.playoverwatch.com/img/pages/heroes/tracer/portrait.png",
+  genji: "https://static.playoverwatch.com/img/pages/heroes/genji/portrait.png",
+  sombra:
+    "https://static.playoverwatch.com/img/pages/heroes/sombra/portrait.png",
+  mercy: "https://static.playoverwatch.com/img/pages/heroes/mercy/portrait.png",
+  winston:
+    "https://static.playoverwatch.com/img/pages/heroes/winston/portrait.png",
+};
